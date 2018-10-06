b6e9c9dac0b3d82c06cc2ed6386fd78dd2a0f133
Viktor.Carlsson <viktor.carlsson@elev.ga.lbs.se>
2018-10-06 16:42:43 +0200

    Klar E

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [Range(-1000, 1000)]
+    [Range(0, 1000)]
     public float rot;
     public SpriteRenderer rend;
     public Color sColor;
-    public int timer;
+    public float timer;
     public int speed = 20;
+    public int x;
+    [Range(0, 70)]
+    public int right;
+    public int newColor;
+    public float ett;
+    float två;
+    float tre;
     
 
     // Use this for initialization
@@ -21,35 +28,70 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        
+
+        
         if (Input.GetKey(KeyCode.D))
         {
-            rend.color = sColor;
+
             rend.color = new Color(0f, 0f, 1f);
-            
-            transform.Rotate(0f, 0f,-rot * Time.deltaTime);
+            //det här gör så att så fort man trycker på D så ändras färgen till helblå
+
+            transform.Rotate(0f, 0f, -rot * Time.deltaTime);
+            //Rotate är så att den kan rotera. och jag har bara i rot. Z. så den kan röra sig upp eller ner.
+            //inte åt dem andra hållen som X och Y
+            //-rot gör så man går rot. åt höger.
             transform.Translate(5f * Time.deltaTime, 0, 0, Space.Self);
+            //Trans. gör så att den rör sig åt ett håll hela tiden.
+            //space.Self gör så att den vet vad som är höger och vänster
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rend.color = sColor;
-            rend.color = new Color(0f,1f,0f);
-            transform.Rotate(0f,0f, rot * Time.deltaTime);
-         transform.Translate(5f * Time.deltaTime, 0, 0, Space.Self);
+            rend.color = new Color(0f, 1f, 0f);
+
+            transform.Rotate(0f, 0f, right * Time.deltaTime);
+            //rot(+) gör så att den åker åt andra hållet.
+            //jag har right här för att den ska inte åka lika snabbt som rot
+            transform.Translate(5f * Time.deltaTime, 0, 0, Space.Self);
         }
-       if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            rot -= 150;
-            speed -= 10;
+            rot /= 2;
+            //den gör så att snabbheten på rot halveras 
+            speed /= 2;
+            //speed är hur snabbt den åker utan att hålla ner något. och den delas på hälften.
 
         }
-       if (Input.GetKeyUp(KeyCode.S))
+        if (Input.GetKeyUp(KeyCode.S))
         {
-            rot += 150;
-            speed += 10;
+            rot *= 2;
+            speed *= 2;
+            //gör så att allt kommer tillbaka när man släpper S.
         }
-        transform.Translate(speed* Time.deltaTime, 0, 0, Space.Self);
-        timer += 1;
-        print("test");
+        transform.Translate(speed * Time.deltaTime, 0, 0, Space.Self);
+        //den gör så att den alltid kommer att åka framåt. Time.deltatime gör så att den åker med sekund
+        //istället för fps.
+        timer += Time.deltaTime;
+        if (timer > x)
+        {
+            x = x + 1;
+            //varje gång X blir större än timern så plusas det föra talet med ett 
+            print("timer: " + x);
+            //att man ser timern i konsolen.
+        }
+        //C nivå.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rend.color = new Color(ett, två, tre);
+            ett = Random.Range(0f, 1f);
+            två = Random.Range(0f, 1f);
+            tre = Random.Range(0f, 1f);
+            //så.. jag vet inte om jag har gjort rät. men vad som händer är att varje gång du trycker space
+            //så får ett, två och tre ett nytt värde. Det gör så att varje gång du trycker space får du en ny färg.
+            //det jag inte förstår är om den ska alltid vara synlig. eller den ska bara finnas där om man inte trycker ner A eller D.
+            //annars så gör man ett nytt script och lägger det på en annan del på skeppet(kolla part1 (1)).
+        }
     }
 
 }
---
+++ Assets/Rfärg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rfärg : MonoBehaviour
+{
+    float ett;
+    float två;
+    float tre; public
+    SpriteRenderer rend;
+    public bool färgPAnnanDel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (färgPAnnanDel == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rend.color = new Color(ett, två, tre);
+                ett = Random.Range(0f, 1f);
+                två = Random.Range(0f, 1f);
+                tre = Random.Range(0f, 1f);
+            }
+        }
+    }
+}
